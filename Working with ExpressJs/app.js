8c97a35723659ea5590c77772867e8f237f242aa
fg227 <fg227@njit.edu>
2025-02-17 17:49:03 +0000

    Handling different routes and parsing incoming request also adding body parser

--- Working with ExpressJs/app.js
+++ Working with ExpressJs/app.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req,res, next)=>{
-    console.log("middleware");
-    next();
-})
-
+app.use(bodyParser.urlencoded())
 
 app.get("/", (req, res)=>{
     res.send("hello world");
 })
+
+app.get('/add_product', (req, res)=>{
+    res.send(`
+            <form action="/product" method="POST">
+                <input type="text" name="title">
+                <button type="submit">Add Product</button>
+            </form>
+    `);
+})
+
+app.use('/product', (req, res, next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
 app.listen(3000);
