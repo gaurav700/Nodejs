62e5cb794fcdd06dae609076709f6e84566ee18c
fg227 <fg227@njit.edu>
2025-02-17 18:11:47 +0000

    Serving HTML pages using sendFile and use path module to get the directory path and join it with the html page location

--- Working with ExpressJs/routes/admin.js
+++ Working with ExpressJs/routes/admin.js
@@ -1,14 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 router.get('/add_product', (req, res)=>{
-    res.send(`
-            <form action="/admin/product" method="POST">
-                <input type="text" name="title">
-                <button type="submit">Add Product</button>
-            </form>
-    `);
+    res.sendFile(path.join(__dirname, '../' , 'views', 'add-product.html'))
 })
 
 router.use('/product', (req, res, next)=>{
--- Working with ExpressJs/routes/shop.js
+++ Working with ExpressJs/routes/shop.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
+const path = require('path');
 const router = express.Router();
 
 router.get("/", (req, res)=>{
-    res.send("hello world");
+    res.sendFile(path.join(__dirname, '../' , 'views', 'shop.html'))
 })
 
 
