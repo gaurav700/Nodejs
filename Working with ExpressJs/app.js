4cc2bfab5907f30f9de429a65fffd70c166f8aaf
fg227 <fg227@njit.edu>
2025-02-17 17:22:52 +0000

    Adding middleware

--- Working with ExpressJs/app.js
+++ Working with ExpressJs/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.use((req,res, next)=>{
+    console.log("middleware");
+    next();
+})
 
+
+app.get("/", (req, res)=>{
+    res.send("hello world");
+})
 app.listen(3000);
