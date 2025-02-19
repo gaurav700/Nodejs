2a58dd6193d90fefe05551ac5c7498177b0eb8c7
fg227 <fg227@njit.edu>
2025-02-19 14:40:50 +0000

    Adding shop.pug file for dynamic content using template engines

--- Working with ExpressJs/app.js
+++ Working with ExpressJs/app.js
@@ -6,9 +6,11 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const rootDir = require('./util/path');
 
+app.set('view engine', 'pug');
+app.set('views', 'views')
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended:false}))
-app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 
--- Working with ExpressJs/routes/admin.js
+++ Working with ExpressJs/routes/admin.js
@@ -1,18 +1,23 @@
-const express = require('express');
 const path = require('path');
-const router = express.Router();
+
+const express = require('express');
 
 const rootDir = require('../util/path');
 
+const router = express.Router();
 
-router.get('/add_product', (req, res)=>{
-    res.sendFile(path.join(rootDir , 'views', 'add-product.html'))
-})
+const products = [];
 
-router.use('/product', (req, res, next)=>{
-    console.log(req.body);
-    res.redirect('/');
-})
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+});
 
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+});
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
