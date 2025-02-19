02d0ad59781e462c52b8a3234f05dd9ff1286956
fg227 <fg227@njit.edu>
2025-02-19 16:29:48 +0000

    Adding ejs template and includes files to reduce redundant code

--- Working with ExpressJs/app.js
+++ Working with ExpressJs/app.js
@@ -8,11 +8,14 @@ const expressHbs = require('express-handlebars');
 const rootDir = require('./util/path');
 
 // for enabling hbs
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
-app.set('view engine', 'hbs');
+// app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
+// app.set('view engine', 'hbs');
 
 // for enabling pug
 // app.set('view engine', 'pug');
+
+// for enabling ejs
+app.set('view engine', 'ejs');
 
 app.set('views', 'views')
 
