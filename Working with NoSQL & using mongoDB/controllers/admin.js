e20a13d5f93be5101adba68e95ac1289d370d0e0
fg227 <fg227@njit.edu>
2025-02-21 22:34:25 +0000

    adding mongodb database config files to the code

--- Working with NoSQL & using mongoDB/controllers/admin.js
+++ Working with NoSQL & using mongoDB/controllers/admin.js
@@ -9,7 +9,23 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  req.user.createProduct({
+    title: title,
+    price: price,
+    description: description,
+    imageUrl: imageUrl
+  })
+  .then(result => {
+    console.log(result);
+    res.redirect('/admin/products')
+  })
+  .catch(err => {
+    console.log(err);
+  })
 };
 
 exports.getEditProduct = (req, res, next) => {
--- Working with NoSQL & using mongoDB/util/database.js
+++ Working with NoSQL & using mongoDB/util/database.js
@@ -1,11 +1,34 @@
-const mysql = require('mysql2');
+require('dotenv').config();
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const mongoUser = process.env.MONGO_USER;
+const mongoPassword = process.env.MONGO_PASSWORD;
+const mongoDbName = process.env.MONGO_DATABASE_NAME;
+
+let _db;
+
+const uri = `mongodb+srv://${mongoUser}:${mongoPassword}@project0.35tzr.mongodb.net/?retryWrites=true&w=majority&appName=${mongoDbName}`;
+const mongoConnect = (callback)=>{
+    MongoClient.connect(uri)
+    .then(client => {
+        console.log("Connected to database...")
+        _db = client.db();
+        callback()
+        }
+    )
+    .catch(err => {
+        console.log(err)
+        throw err;
+    });
+}
+
+const getDb = ()=>{
+    if(_db){
+        return _db;
+    }
+    throw err;
+}
 
 
-const pool = mysql.createPool({
-    host : 'localhost',
-    user : 'root',
-    database : 'node-complete',
-    password : 6342
-})
-
-module.exports = pool.promise();
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
