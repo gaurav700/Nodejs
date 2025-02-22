d240c86355db6b359f7dd1e712293fdea74fa405
fg227 <fg227@njit.edu>
2025-02-22 01:03:59 +0000

    Finishing up adding add orders and get orders

--- Working with NoSQL & using mongoDB/controllers/shop.js
+++ Working with NoSQL & using mongoDB/controllers/shop.js
@@ -88,3 +88,24 @@ exports.getCheckout = (req, res, next) => {
     pageTitle: 'Checkout'
   });
 };
+
+exports.postOrder = (req,res, next) =>{
+  req.user.addOrder()
+  .then(result => {
+    res.redirect('/orders');
+  })
+  .catch(err => console.log(err));
+}
+
+exports.getOrders = (req, res, next) => {
+  req.user
+    .getOrders()
+    .then(orders => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders
+      });
+    })
+    .catch(err => console.log(err));
+};
