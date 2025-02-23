f1f5d497a41f7852e37af92209e718aa5a957ce1
fg227 <fg227@njit.edu>
2025-02-23 21:23:03 +0000

    initital starter proejct for mongooose

---
+++ Working with mongoose/controllers/error.js
@@ -0,0 +1,4 @@
+exports.get404 = (req, res, next)=>{
+    // res.sendFile(path.join(rootDir , 'views', '404.html'))
+    res.status(404).render('404', {pageTitle : '404 | Page not found', path : '/404'});
+}
