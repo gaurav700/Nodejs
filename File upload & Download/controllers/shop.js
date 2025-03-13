f1f3ab45a3c7ec7cec676559d5cec09729ca3985
fg227 <fg227@njit.edu>
2025-03-13 17:11:43 +0000

    Using pdfkit for generating pdf

--- File upload & Download/controllers/shop.js
+++ File upload & Download/controllers/shop.js
@@ -2,6 +2,7 @@ const Product = require('../models/product');
 const Order = require('../models/order');
 const fs = require('fs');
 const path = require('path');
+const PDFDocument = require('pdfkit');
 exports.getProducts = (req, res, next) => {
   Product.find()
     .then(products => {
@@ -151,13 +152,44 @@ exports.getOrders = (req, res, next) => {
 
 exports.getInvoice = (req, res, next)=>{
   const orderId = req.params.orderId;
-  const invoiceName = 'invoice-'+orderId + '.pdf';
-  fs.readFile(invoiceName, (err, data) => {
-    if(err){
-      return next(err);
+  Order.findById(orderId)
+  .then(order => {
+    if (!order) {
+      return next(new Error('No order found.'));
     }
+    if (order.user.userId.toString() !== req.user._id.toString()) {
+      return next(new Error('Unauthorized'));
+    }
+    const invoiceName = 'invoice-' + orderId + '.pdf';
+    const invoicePath = path.join('data', 'invoices', invoiceName);
+    const pdfDoc = new PDFDocument();
     res.setHeader('Content-Type', 'application/pdf');
-    res.setHeader('Content-Disposition', 'inline  ; filename="' + invoiceName + '"');
-    res.send(data);
-  });
+    res.setHeader(
+      'Content-Disposition',
+      'inline; filename="' + invoiceName + '"'
+    );
+    pdfDoc.pipe(fs.createWriteStream(invoicePath));
+    pdfDoc.pipe(res);
+    pdfDoc.fontSize(26).text('Invoice');
+    pdfDoc.text('-----------------------');
+    let totalPrice = 0;
+    order.products.forEach(prod => {
+      totalPrice += prod.quantity * prod.product.price;
+      pdfDoc
+        .fontSize(14)
+        .text(
+          prod.product.title +
+          ' - ' +
+          prod.quantity +
+          ' x ' +
+          '$' +
+          prod.product.price
+        );
+    });
+    pdfDoc.text('-----------------------');
+    pdfDoc.fontSize(20).text('Total Price: $' + totalPrice);
+
+    pdfDoc.end();
+  })
+  .catch(err => next(err));
 }
