0d9e927d8549fea49d446627136b9e0041074521
fg227 <fg227@njit.edu>
2025-03-15 23:28:41 +0000

    adding stripe for payment interface

--- Payment/controllers/shop.js
+++ Payment/controllers/shop.js
@@ -1,8 +1,12 @@
+require("dotenv").config();
+const stripeKey = encodeURIComponent(process.env.STRIPE_SECRET_KEY);
 const Product = require('../models/product');
 const Order = require('../models/order');
 const fs = require('fs');
 const path = require('path');
 const PDFDocument = require('pdfkit');
+const stripe = require('stripe')(stripeKey);
+
 exports.getProducts = (req, res, next) => {
   Product.find()
     .then(products => {
@@ -199,4 +203,54 @@ exports.getInvoice = (req, res, next) => {
       pdfDoc.end();
     })
     .catch(err => next(err));
+};
+
+
+
+exports.getCheckout = (req, res, next) => {
+  let products;
+  let total = 0;
+  req.user
+    .populate('cart.items.productId')
+    // .execPopulate() 
+    .then(user => {
+      products = user.cart.items.filter(item => item.productId !== null);
+      total = 0;
+      products.forEach(p => {
+        total += p.quantity * p.productId.price;
+      });
+      return stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: products.map(p => {
+          return {
+            price_data: {
+              currency: 'usd',
+              product_data: {
+                name: p.productId.title,
+                description: p.productId.description,
+              },
+              unit_amount: Math.round(p.productId.price * 100),
+            },
+            quantity: p.quantity
+          };
+        }),
+        mode: 'payment',
+        success_url: req.protocol + '://' + req.get('host') + '/checkout/success',
+        cancel_url: req.protocol + '://' + req.get('host') + '/checkout/cancel'
+      });
+    })
+    .then(session => {
+      res.render('shop/checkout', {
+        path: '/checkout',
+        pageTitle: 'Checkout',
+        products: products,
+        totalSum: total,
+        sessionId: session.id
+      });
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
